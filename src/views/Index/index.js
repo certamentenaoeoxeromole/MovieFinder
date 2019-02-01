d8ef3f82256a8162f7238eec7ef4382038e4a65e
xeromole <tqbrasil10@gmail.com>
2019-01-31 21:56:03 -0300

    Adicionado babel presets plugins, e continuacao de criacao de estrutura

---
+++ src/views/Index/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from "react";
+import Header from "~/components/Header/index";
+
+const Index = () => (
+  <Fragment>
+    <Header />
+  </Fragment>
+);
+
+export default Index;
