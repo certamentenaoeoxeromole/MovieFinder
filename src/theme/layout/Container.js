fd402cbb75f693f6e6e2239043d0ecd095955fc5
xeromole <tqbrasil10@gmail.com>
2019-02-04 10:37:36 -0300

    ajustando bug container

--- src/theme/layout/Container.js
+++ src/theme/layout/Container.js
@@ -8,10 +8,10 @@ const Container = styled.div`
   padding-left: 10px;
   padding-right: 10px;
 
-  @media (min-width: ${media.screenMdMin}) {
+  @media (min-width: 768px) {
     width: 740px;
   }
-  @media (min-width: ${media.screeenLgMin}) {
+  @media (min-width: 992px) {
     width: 960px;
   }
   @media (min-width: 1280px) {
